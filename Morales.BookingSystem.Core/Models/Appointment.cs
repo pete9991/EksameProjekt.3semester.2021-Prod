82c96d8a7915af1da840d4ed21fe4f9c438647ca
Morales-10 <anna450s@easv365.dk>
2021-11-22 15:39:41 +0100

    core/models

---
+++ Morales.BookingSystem.Core/Models/Appointment.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+    public class Appointment
+    {
+        public int id { get; set; }
+        public int customerid { get; set; }
+        public int employeeid { get; set; }
+        public string type { get; set; }
+        public float date { get; set; }
+        public int duration { get; set; }
+        private List<int> treatments;
+    }
+}
+    
---
+++ Morales.BookingSystem.Core/Models/Treatments.cs
@@ -0,0 +1,8 @@
+namespace Core.Models
+{
+    public class Treatments
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
